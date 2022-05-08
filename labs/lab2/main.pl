fda714240be4f5bbd496d606130c6da329d86763
SimonHotDog <simon.lu@live.se>
2022-05-08 19:27:45 +0200

    Lab 2 almost done

--- labs/lab2/main.pl
+++ labs/lab2/main.pl
@@ -1,63 +1,57 @@
-ins(X, [], [X]) :- !.
-ins(
-    sublist(S1, I1, J1, L1),
-    [sublist(S2, I2, J2, L2)|T],
-    [sublist(S1, I1, J1, L1), sublist(S2, I2, J2, L2)|T]) :- S1 =< S2, !.
-ins(X, [X1|T1], [X2,T2]) :- ins(X, T1, T2).
+% Simon Lundberg
 
-insertionSort([],[]).
-insertionSort([X,T], T2) :- insertionSort(T, T1), ins(X, T1, T2).
+%ins(X, [], [X]) :- !.
+%ins(X1, [X2|L], [X1, X2|L]) :-
+%    X1 = sublist(S1,_,_,_),
+%    X2 = sublist(S2,_,_,_),
+%    S1 =< S2, !.
+%ins(X1, [X2|L], [X2|L1]) :- ins(X1, L, L1).
+%
+%insertionSort([],[]) :- !.
+%insertionSort([X,L], S) :- insertionSort(L, S1), ins(X, S1, S).
+
+sorting(I, O) :- sort(1, @=<, I, O).
 
 sum(L, S) :- sum_list(L, S).
 
-% Sublist generation
-
 generate([_], []).
-generate([H | T], [H | O]) :- generate(T, O).
+generate([X | T], [X | O]):-
+    generate(T, O).
 
 sublistsInner([], _, []).
-sublistsInner([In, T], I, [[S, I, J, [In | T]] | XS]) :-
-    generate([In | T], Gen),
-    sum([In | T], S),
+sublistsInner([X | T], I, [[S, I, J, [X | T]] | XS]) :-
+    generate([X | T], Gen),
+    sum([X | T], S),
     sublistsInner(Gen, I, XS),
-    length([In | T], L),
+    length([X | T], L),
     J is I + L - 1.
 
+
 sublists([], _, []).
-sublists([In | T], I, [Inner | XS]) :-
-    J is I + 1,
-    sublistsInner([In | T], I, Inner),
+sublists([X | T], I, [Inner | XS]) :-
+    J is I+1,
+    sublistsInner([X | T], I, Inner),
     sublists(T, J, XS).
 
-% Final part
-
 smallestK(0, _, []).
-smallestK(K, [L | LS], [L | XS]) :-
+smallestK(K, [List | Lists], [List | Xs]) :-
     L is K-1,
-    smallestK(L, LS, XS).
+    smallestK(L, Lists, Xs).
 
-% Printing mess
 
-toStringLine([S1, I, J, X], Out) :-
-    string_concat(S1, "\t", S2),
-    string_concat(S2, I, S3),
-    string_concat(S3, "\t", S4),
-    string_concat(S4, J, S5),
-    string_concat(S5, "\t", S6),
-    atomics_to_string(X, ",", SX),
-    string_concat(S6, SX, S7),
-    string_concat(S7, "/n", Out).
+toStringInner([S, I, J, X]) :-
+    write(S), write('\t'), write(I), write('\t'), write(J), write('\t'), write(L), write('\n').
 
 toString([]).
 toString([X | XS]) :-
-    toStringLine(X, Out),
-    write(Out),
+    toStringInner(X),
     toString(XS).
+
 
 testTemplate(K, L) :-
     sublists(L, 1, X),
-    append(X, Y),
-    insertionSort(Y, Sorted),
+    append(X,Y),
+    sorting(Y, Sorted),
     smallestK(K, Sorted, Out),
     write("Size, \t Start \t End \t List\n"),
     toString(Out).
