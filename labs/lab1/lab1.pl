c290c1f1c281bda870d021c5db1a5e38f30ac80d
SimonHotDog <simon.lu@live.se>
2022-05-16 08:44:23 +0200

    Minor refactoring and comment clarification

--- labs/lab1/lab1.pl
+++ labs/lab1/lab1.pl
@@ -4,33 +4,9 @@
 % Every room has [Loot], which contain the items in the room.
 %
 % state(
-%     robot(position, [Inventory]),
-%     Room1([Loot]),
-%     Room2([Loot]),
-%     Room3([Loot])
+%     robot(position, [Inventory]), [Room1Loot], [Room2Loot], [Room3Loot]
 % )
 
-% Movement actions:
-
-proceed(
-    state(robot(1, Inventory), Room1, Room2, Room3), % Moves from room 1 to room 2
-    move(2),
-    state(robot(2, Inventory), Room1, Room2, Room3)) :- member(steel, Inventory). 
-
-proceed(
-    state(robot(2, Inventory), Room1, Room2, Room3), % Moves from room 2 to room 1
-    move(1),
-    state(robot(1, Inventory), Room1, Room2, Room3)) :- member(steel, Inventory). 
-
-proceed(
-    state(robot(1, Inventory), Room1, Room2, Room3), % Moves from room 1 to room 3
-    move(3),
-    state(robot(3, Inventory), Room1, Room2, Room3)) :- member(brass, Inventory). 
-
-proceed(
-    state(robot(3, Inventory), Room1, Room2, Room3), % Moves from room 3 to room 1
-    move(1),
-    state(robot(1, Inventory), Room1, Room2, Room3)) :- member(brass, Inventory). 
 
 % Pick up item actions:
 
@@ -65,6 +41,29 @@ proceed(
     state(robot(3, Pre_Inventory), Room1, Room2, Room3), % Drops item while in room 3
     drop(Item),
     state(robot(3, Post_Inventory), Room1, Room2, [Item|Room3])) :- member(Item, Pre_Inventory), delete(Pre_Inventory, Item, Post_Inventory).
+
+% Movement actions:
+
+proceed(
+    state(robot(1, Inventory), Room1, Room2, Room3), % Moves from room 1 to room 2
+    move(2),
+    state(robot(2, Inventory), Room1, Room2, Room3)) :- member(steel, Inventory). 
+
+proceed(
+    state(robot(2, Inventory), Room1, Room2, Room3), % Moves from room 2 to room 1
+    move(1),
+    state(robot(1, Inventory), Room1, Room2, Room3)) :- member(steel, Inventory). 
+
+proceed(
+    state(robot(1, Inventory), Room1, Room2, Room3), % Moves from room 1 to room 3
+    move(3),
+    state(robot(3, Inventory), Room1, Room2, Room3)) :- member(brass, Inventory). 
+
+proceed(
+    state(robot(3, Inventory), Room1, Room2, Room3), % Moves from room 3 to room 1
+    move(1),
+    state(robot(1, Inventory), Room1, Room2, Room3)) :- member(brass, Inventory). 
+
 
 % Solving the puzzle
 
